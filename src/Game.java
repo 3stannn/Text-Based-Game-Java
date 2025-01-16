7e78f64720f1eff72c6247cb7dca0769d2529b3f
thristan <thristandave12@gmail.com>
2025-01-16 14:15:25 +0800

    Add game screen functionality and title screen handler

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,28 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Game {
 
     JFrame window; // import javax.swing.JFrame;
     Container container; // import java.awt.Container;
-    JPanel titleNamepanel, startButtonPanel; // import javax.swing.JPanel;
+    JPanel titleNamepanel, startButtonPanel, mainTextPanel; // import javax.swing.JPanel;
     JLabel titleNameLabel; // import javax.swing.JLabel;
     JButton startButton; // import javax.swing.JButton;
+    JTextArea mainTextArea; // import javax.swing.JTextArea;
 
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90); // import java.awt.Font;
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 30); // import java.awt.Font;
+
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
 
     public static void main(String[] args) {
         
@@ -31,7 +37,6 @@ public class Game {
         window.getContentPane().setBackground(Color.BLACK); // import java.awt.Color;
         window.setLayout(null);
         
-
         container = window.getContentPane();
 
         titleNamepanel = new JPanel();
@@ -53,6 +58,9 @@ public class Game {
         startButton.setFocusPainted(false); // removes the outline on start
         startButton.setBorderPainted(true);
 
+        // function
+        startButton.addActionListener(tsHandler);
+
         titleNamepanel.add(titleNameLabel);
         startButtonPanel.add(startButton); // adds the button to the startButtonPanel
         // adds the layer of the panels
@@ -61,5 +69,35 @@ public class Game {
         
         window.setVisible(true);
         window.setResizable(false);
+    }
+
+    public void createGameScreen() {
+
+        titleNamepanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.blue);
+        container.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("This is the main text area. This game is going to be great.");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true); // word wrap to true
+        mainTextPanel.add(mainTextArea);
+        
+    }
+
+    // import java.awt.event.ActionEvent;
+    // import java.awt.event.ActionListener;
+    public class TitleScreenHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            
+            createGameScreen();
+        }
     }
 }
